9ee4856a01c5a6be5b397ffef09b22be18ba8bf0
mehrAd <mjafaripoursws@gmail.com>
2024-06-28 19:04:40 +0330

    index  checked and updated

--- Phase2/Index Files/Create Index.sql
+++ Phase2/Index Files/Create Index.sql
@@ -1,5 +1,5 @@
-CREATE INDEX IF NOT EXISTS datePlacedIdx ON trades(date_placed);
+CREATE INDEX IF NOT EXISTS date_placed_idx ON p2p(date_placed);
 
-CREATE INDEX IF NOT EXISTS walletIdIdx ON transactions(source_wallet_id);
+CREATE INDEX IF NOT EXISTS wallet_id_idx ON transactions(source_wallet_id);
 
-CREATE INDEX IF NOT EXISTS amountIdx ON orders(amount);
+CREATE INDEX IF NOT EXISTS amount_idx ON orders(amount);

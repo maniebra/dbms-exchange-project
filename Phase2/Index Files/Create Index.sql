481f715db6e8d023b88ccc3eb2d53b32e28ffb62
mehrAd <mjafaripoursws@gmail.com>
2024-06-29 00:20:45 +0330

    amont changed with fill in indexfiles

--- Phase2/Index Files/Create Index.sql
+++ Phase2/Index Files/Create Index.sql
@@ -2,4 +2,4 @@ CREATE INDEX IF NOT EXISTS date_placed_idx ON p2p(date_placed);
 
 CREATE INDEX IF NOT EXISTS wallet_id_idx ON transactions(source_wallet_id);
 
-CREATE INDEX IF NOT EXISTS amount_idx ON orders(amount);
+CREATE INDEX IF NOT EXISTS fill_idx ON orders(fill);
